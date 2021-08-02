1aab885cf991307b51d82f2856f710934c43b7c0
Amar Spahić <amar.spahic@intellegens.hr>
2021-08-02 10:23:49 +0200

    Fixed hover icon div being placed outside of image

--- src/components/Body/Showcase/SingleProject.js
+++ src/components/Body/Showcase/SingleProject.js
@@ -1,64 +1,75 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import LoginData from '../LoginData/LoginData';
-import TileIcons from './tileIcons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import LoginData from "../LoginData/LoginData";
+import TileIcons from "./tileIcons";
 
 const SingleProject = (props) => {
-    const handleCopy = (e, id, value) => {
-        e.stopPropagation();
+  const handleCopy = (e, id, value) => {
+    e.stopPropagation();
 
-        const copyId = document.getElementById(id);
-        copyId.select();
-        document.execCommand('Copy');
-        copyId.value = "Copied";
+    const copyId = document.getElementById(id);
+    copyId.select();
+    document.execCommand("Copy");
+    copyId.value = "Copied";
 
-        copyId.style.backgroundColor = "#3A3856";
-        copyId.style.color = "#E0E0E0";
-        setTimeout(() => {
-            copyId.value = value;
-            copyId.style.backgroundColor = "#E0E0E0";
-            copyId.style.color = "#000";
-        }, 1000)
+    copyId.style.backgroundColor = "#3A3856";
+    copyId.style.color = "#E0E0E0";
+    setTimeout(() => {
+      copyId.value = value;
+      copyId.style.backgroundColor = "#E0E0E0";
+      copyId.style.color = "#000";
+    }, 1000);
+  };
+
+  const handlePictureHover = (id, e) => {
+    const tile = document.getElementById(`project-tile-hover-${id}`);
+    if (e.type === "mouseenter") {
+      tile.classList.remove("project-tile-hidden");
+      tile.classList.add("project-tile-shown");
+    } else if (e.type === "mouseleave") {
+      tile.classList.remove("project-tile-shown");
+      tile.classList.add("project-tile-hidden");
     }
+  };
 
-    const handlePictureHover = (id, e) => {
-        const tile = document.getElementById(`project-tile-hover-${id}`);
-        if(e.type === "mouseenter"){
-            tile.classList.remove("project-tile-hidden");
-            tile.classList.add("project-tile-shown");
-        } else if(e.type === "mouseleave"){
-            tile.classList.remove("project-tile-shown");
-            tile.classList.add("project-tile-hidden");
-        }
-    }
-
-    return (
-        <div className="project-container">
-            <div className="project-info">
-                <div className="project-title">
-                    <a href={props.project.link} target="_blank" rel="noreferrer">
-                        {props.project.title}
-                    </a>
-                </div>
-                <div className="project-header">{props.project.header}</div>
-               {props.project.login ?  <LoginData data={props.project.loginData} handleCopy={handleCopy} /> : null}
-                <div className="project-text">{props.project.text}</div>
-                <div className="project-link">
-                    <a href={props.project.link} target="_blank" rel="noreferrer">
-                        <span>Visit {props.project.title}</span>
-                        <FontAwesomeIcon icon={faArrowRight} />
-                    </a>
-                </div>
-            </div>
-            <div className="project-picture" onMouseEnter={handlePictureHover.bind(null, props.project.id)} onMouseLeave={handlePictureHover.bind(null, props.project.id)}>
-                <img src={props.project.imgSrc} alt={props.project.title} />
-                <div id={`project-tile-hover-${props.project.id}`} className="project-tile-hover">
-                <TileIcons tiles={props.project.tiles} />
-                </div>
-            </div>
+  return (
+    <div className="project-container">
+      <div className="project-info">
+        <div className="project-title">
+          <a href={props.project.link} target="_blank" rel="noreferrer">
+            {props.project.title}
+          </a>
         </div>
-    );
+        <div className="project-header">{props.project.header}</div>
+        {props.project.login ? (
+          <LoginData data={props.project.loginData} handleCopy={handleCopy} />
+        ) : null}
+        <div className="project-text">{props.project.text}</div>
+        <div className="project-link">
+          <a href={props.project.link} target="_blank" rel="noreferrer">
+            <span>Visit {props.project.title}</span>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </a>
+        </div>
+      </div>
+      <div
+        className="project-picture"
+        onMouseEnter={handlePictureHover.bind(null, props.project.id)}
+        onMouseLeave={handlePictureHover.bind(null, props.project.id)}
+      >
+        <div className="auto">
+          <img src={props.project.imgSrc} alt={props.project.title} />
+          <div
+            id={`project-tile-hover-${props.project.id}`}
+            className="project-tile-hover"
+          >
+            <TileIcons tiles={props.project.tiles} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleProject;
